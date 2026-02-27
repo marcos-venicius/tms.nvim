6318c94ab059ac55cce0a9534ced841884a47ff2
Marcos Venicius <marcosdevelopment.business@gmail.com>
2026-02-27 08:32:47 -0300

    make TMSNextTheme available

--- README.md
+++ README.md
@@ -10,10 +10,35 @@ My plugin to select themes dynamically
     name = "tms",
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function ()
-        require("tms").setup({
+        local m = require("tms")
+
+        m.setup({
             "theme1",
             "theme2",
         })
     end
 }
 ```
+
+Now you can alose have a list of themes but disable theme cycling and select next theme when you want.
+
+```lua
+{
+    "marcos-venicius/tms.nvim",
+    name = "tms",
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function ()
+        local m = require("tms")
+
+        -- disable theme cycling
+        m.disableThemeCycling()
+
+        m.setup({
+            "theme1",
+            "theme2",
+        })
+    end
+}
+```
+
+So, at any time during your editor usage, if you want to select the next theme just call: `TMSNextTheme` and the next theme will be selected.
--- lua/tms/init.lua
+++ lua/tms/init.lua
@@ -1,7 +1,12 @@
 local Path = require('plenary.path')
 local storage_dir = vim.fn.stdpath('data') .. '/tms/'
 local current_theme_file = storage_dir .. 'current-theme'
-local M = {}
+local M = {
+  configs = {
+    disableThemeCycling = false
+  },
+  themes = {}
+}
 
 local function get_current_theme()
   local path = Path:new(current_theme_file)
@@ -21,23 +26,60 @@ local function save_current_theme(theme)
   path:write(tostring(theme), 'w')
 end
 
+function M.disableThemeCycling()
+  M.configs.disableThemeCycling = true
+end
+
+function M.select_next_theme()
+  if #M.themes == 0 then
+    vim.notify("TMS: no themes configured", vim.log.levels.WARN)
+    return
+  end
+
+  local theme_index = get_current_theme()
+
+  if theme_index > #M.themes then
+    theme_index = 1
+  else
+    theme_index = theme_index + 1
+  end
+
+  local theme = M.themes[theme_index]
+
+  vim.notify("using \"" .. theme .. "\" theme", vim.log.levels.INFO)
+
+  vim.cmd.colorscheme(theme)
+
+  save_current_theme(theme_index)
+end
+
 function M.setup(themes)
+  vim.api.nvim_create_user_command("TMSNextTheme", function()
+    M.select_next_theme()
+  end, {})
+
   themes = themes or {}
 
   if #themes == 0 then
     return
   end
 
-  local theme_index = get_current_theme()
+  M.themes = themes
 
-  if theme_index > #themes then
-    theme_index = 1
+  local theme_index = get_current_theme()
+  local next_theme = theme_index
+
+  if not M.configs.disableThemeCycling then
+    if theme_index > #themes then
+      theme_index = 1
+    end
+
+    next_theme = theme_index + 1
   end
 
   local theme = themes[theme_index]
-  local next_theme = theme_index + 1
 
-  print("using \"" .. theme .. "\" theme")
+  vim.notify('using "' .. theme .. '" theme', vim.log.levels.INFO)
 
   vim.cmd.colorscheme(theme)
 
